bc41905fed487a39ff7c9a3f9fca14666b1b94b4
Abhishek <you@example.com>
2022-08-05 10:18:04 +0530

    Added [Abhishek]: Welcome to line comparision

---
+++ src/com/bridgelabz/lineComparision/LineComparision.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("***Welcome to the Line Comparision Computation program***");
+
+	}
+
+}
